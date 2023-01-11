603a16ceb32d1dea2bbcfc6223f3f100eafab5c1
Douglas <cbdouglasvirissimo@hotmail.com>
2023-01-11 15:20:20 -0300

    1º Requisito

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import logo from './trivia.png';
+import { Route, Switch } from 'react-router';
+import Login from './pages/Login';
+// import logo from './trivia.png';
 import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>SUA VEZ</p>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+    </Switch>
   );
 }
+
+export default App;
